373bcfa978fbbfc966b1d2dd96d17e158843c991
Bilkis Ismail <bilkisismail07@gmail.com>
2019-08-11 21:19:08 +0700

    Chart and Counting Periode

--- public/guest/result.php
+++ public/guest/result.php
@@ -22,11 +22,65 @@ if(isset($_POST['run'])){
     while( $row = mysqli_fetch_assoc($result) ) {
         $rows[] = $row;
     }
+
+     $month = array(
+      0 => 0, //jan
+      1 => 0, //feb
+      2 => 0, //mar
+      3 => 0, //apr
+      4 => 0, //may
+      5 => 0, //jun
+      6 => 0, //jul
+      7 => 0, //aug
+      8 => 0, //sep
+      9 => 0, //oct
+      10 => 0, //nov
+      11 => 0 //des
+    );
+
+    for ($b=0; $b < count($rows); $b++) { 
+      if ($rows[$b]['month'] == 'Jan') {
+        $month[0]++;
+      } 
+      if ($rows[$b]['month'] == 'Feb'){
+        $month[1]++;
+      }
+      if ($rows[$b]['month'] == 'Mar') {
+        $month[2]++;
+      } 
+      if ($rows[$b]['month'] == 'Apr'){
+        $month[3]++;
+      }
+      if ($rows[$b]['month'] == 'May') {
+        $month[4]++;
+      } 
+      if ($rows[$b]['month'] == 'Jun'){
+        $month[5]++;
+      }
+      if ($rows[$b]['month'] == 'Jul') {
+        $month[6]++;
+      } 
+      if ($rows[$b]['month'] == 'Aug'){
+        $month[7]++;
+      }
+      if ($rows[$b]['month'] == 'Sep'){
+        $month[8]++;
+      }
+      if ($rows[$b]['month'] == 'Oct'){
+        $month[9]++;
+      }
+      if ($rows[$b]['month'] == 'Nov'){
+        $month[10]++;
+      }
+      if ($rows[$b]['month'] == 'Dec'){
+        $month[11]++;
+      }
+    }
     
+
 } else {
     header('Location: ../../index.php');
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -100,15 +154,21 @@ if(isset($_POST['run'])){
           <li class="breadcrumb-item"><a href="../home">Back</a></li>
         </ol>
 
-        <!-- add modal -->
-        <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal">
-          Create New Produk
-        </button>
+        <!-- Area Chart Example-->
+        <div class="card mb-3">
+          <div class="card-header">
+            <i class="fas fa-chart-area"></i>
+            Statistik</div>
+          <div class="card-body">
+            <canvas id="myAreaChart" width="100%" height="30"></canvas>
+          </div>
+          <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+        </div>
 
         <div class="card mb-3" id="card">
             <div class="card-header">
             <i class="fas fa-table"></i>
-            Users Table</div>
+            Result</div>
             <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -122,20 +182,34 @@ if(isset($_POST['run'])){
                     </thead>
                     <tbody>
                         <?php $i=1; foreach($rows as $row) : ?>
-                        <?php 
-                            $z = $row['month'];
-                            $all = mysqli_query($conn, "SELECT COUNT(`month`) FROM upload_data WHERE 
-                                    `month` = '$z' AND 
-                                    product_id = '$produk' AND
-                                    kategori = '$kategori' AND
-                                    created_at >= '$start' AND 
-                                    created_at <= '$end' 
-                                    ");
-                            $alls = mysqli_fetch_assoc($all);
-                        ?>
                             <tr>
                                 <td><?= $row['month']?></td>
-                                <td><?= $alls['COUNT(`month`)']?></td> 
+                                <!-- Counting -->
+                                <?php if($row['month'] == 'Jan') : ?>
+                                <td><?= $month[0] ?></td> 
+                                <?php elseif($row['month'] == 'Feb') : ?>
+                                <td><?= $month[1] ?></td>
+                                <?php elseif($row['month'] == 'Mar') : ?>
+                                <td><?= $month[2] ?></td>
+                                <?php elseif($row['month'] == 'Apr') : ?>
+                                <td><?= $month[3] ?></td>
+                                <?php elseif($row['month'] == 'May') : ?>
+                                <td><?= $month[4] ?></td>
+                                <?php elseif($row['month'] == 'Jun') : ?>
+                                <td><?= $month[5] ?></td>
+                                <?php elseif($row['month'] == 'Jul') : ?>
+                                <td><?= $month[6] ?></td>
+                                <?php elseif($row['month'] == 'Aug') : ?>
+                                <td><?= $month[7] ?></td>
+                                <?php elseif($row['month'] == 'Sep') : ?>
+                                <td><?= $month[8] ?></td>
+                                <?php elseif($row['month'] == 'Oct') : ?>
+                                <td><?= $month[9] ?></td>
+                                <?php elseif($row['month'] == 'Nov') : ?>
+                                <td><?= $month[10] ?></td>
+                                <?php elseif($row['month'] == 'Dec') : ?>
+                                <td><?= $month[11] ?></td>
+                                <?php endif; ?>
                                 <td><?= $row['product_id']?></td> 
                                 <td><?= $row['kategori']?></td>  
                             </tr>
@@ -188,35 +262,6 @@ if(isset($_POST['run'])){
       </div>
     </div>
   </div>
-
-  <!-- add Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">ADD PRODUCT</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <form action="" method="post">
-          <div class="modal-body">
-            <input type="hidden"value="<?= $_SESSION['data']['id'] ?>" name="id_user" required>  
-            <div class="form-group">
-              <label for="product">Product</label>
-              <input type="text" class="form-control" id="product" placeholder="Product..." name="produk" required>
-            </div>
-            <div class="form-group">
-              <label for="kategori">Kategori</label>
-              <textarea class="form-control" id="kategori" rows="5" name="kategori" required></textarea>
-            </div>
-            <button type="submit" class="w-100 btn btn-primary" name="addProduk">Save Change</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-
   
 
   <!-- Bootstrap core JavaScript-->
@@ -227,13 +272,73 @@ if(isset($_POST['run'])){
   <script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Page level plugin JavaScript-->
+  <script src="../../vendor/chart.js/Chart.min.js"></script>
   <script src="../../vendor/datatables/jquery.dataTables.js"></script>
   <script src="../../vendor/datatables/dataTables.bootstrap4.js"></script>
 
   <!-- Custom scripts for all pages-->
   <script src="../../resources/js/sb-admin.min.js"></script>
    <script src="../../resources/js/demo/datatables-demo.js"></script>
-  <script src="../../resources/js/demo/chart-area-demo.js"></script>
+   <script>
+   // Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+      label: "Sessions",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius:7,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [
+      <?= $month[0] ?>, <?= $month[1] ?>, <?= $month[2] ?>, 
+      <?= $month[3] ?>, <?= $month[4] ?>, <?= $month[5] ?>, 
+      <?= $month[6] ?>, <?= $month[7] ?>, <?= $month[8] ?>, 
+      <?= $month[9] ?>, <?= $month[10] ?>, <?= $month[11] ?>
+      ],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          maxTicksLimit: 10
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+   </script>
 </body>
 
 </html>
